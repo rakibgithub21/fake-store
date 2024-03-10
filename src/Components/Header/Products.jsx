c2ef3b30500bfa7e42952e6bad634653c87b47ae
rakibgithub21 <mdrakiulislam.gff@gmail.com>
2024-03-10 18:14:46 +0600

    display card details

--- src/Components/Header/Products.jsx
+++ src/Components/Header/Products.jsx
@@ -10,21 +10,21 @@ const Products = () => {
             .then(res => res.json())
             .then(data => setProducts(data))
     }, [])
-    
+
     return (
-        <div className="container mx-auto flex">
+        <div className="container mx-auto flex my-20 gap-5">
             <div id="products-container" className="w-4/5">
-                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-10 gap-y-20">
                     {
                         products.map(product => <Product
                             key={product.id}
-                            product= {product}
+                            product={product}
                         ></Product>)
                     }
                 </div>
             </div>
-            <div id="order-summary" className="1/5">
-                <h1>I am order</h1>
+            <div id="order-summary" className="w-1/5 bg-[#ff99004d] p-5 rounded">
+                <h1 className="text-center text-2xl font-semibold border-2 p-2 border-red-300 rounded-xl bg-slate-200">Order Summary</h1>
             </div>
         </div>
     );
--- src/Components/Product/Product.jsx
+++ src/Components/Product/Product.jsx
@@ -5,8 +5,8 @@ const Product = ({ product }) => {
     const { image, title, category, price } = product
     return (
         <div>
-            <div className="card  bg-base-100 shadow-xl">
-                <figure><img className="h-40" src={image} alt="Shoes" /></figure>
+            <div className="card  bg-base-100 shadow-xl border">
+                <figure><img className="h-40 p-2" src={image} alt="Shoes" /></figure>
                 <div className="card-body">
                     <h2 className="card-title">{category}</h2>
                     <p>Price: {price}</p>
